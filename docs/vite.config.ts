8af59ff99f0c0e2255072c8c778cc64ef2566807
Jeff.Qian <qbaojian@163.com>
2024-04-29 10:53:43 +0800

    update docs build

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,4 +1,4 @@
-import { getDocSiteConfig } from 'fe-ent-vue-scripts';
+import { getDocSiteConfig } from 'fe-ent-cli';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 const root = searchForWorkspaceRoot(process.cwd());
@@ -11,36 +11,36 @@ export default defineConfig(({ command, mode }) => {
         minify: false,
         cssCodeSplit: true,
         emptyOutDir: true,
-        outDir: `${root}/dist`,
+        outDir: `${root}/dist`
       },
       server: {
-        port: 5400,
+        port: 5400
       },
       resolve: {
         alias: [
           // 别名，转发 fe-ent-core 文件请求
           {
             find: /^fe-ent-core\/CHANGELOG.md$/,
-            replacement: `${root}/packages/ent-core/CHANGELOG.md`,
+            replacement: `${root}/packages/ent-core/CHANGELOG.md`
           },
           {
             find: /^fe-ent-core\/CHANGELOG.zh-CN.md$/,
-            replacement: `${root}/packages/ent-core/CHANGELOG.zh-CN.md`,
+            replacement: `${root}/packages/ent-core/CHANGELOG.zh-CN.md`
           },
           {
             find: /^fe-ent-core$/,
-            replacement: `${root}/packages/ent-core/src/index.ts`,
+            replacement: `${root}/packages/ent-core/src/index.ts`
           },
           {
             find: /^fe-ent-core\/es\/(.*)$/,
-            replacement: `${root}/packages/ent-core/src/$1`,
+            replacement: `${root}/packages/ent-core/src/$1`
           },
           {
             find: /^fe-ent-layout$/,
-            replacement: `${root}/apps/layout/src/index.ts`,
-          },
-        ],
-      },
-    },
+            replacement: `${root}/apps/layout/src/index.ts`
+          }
+        ]
+      }
+    }
   });
 });
